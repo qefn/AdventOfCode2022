71b58e768c3f41066b31216dc9cc0185b1dfcf12
Sebastian Weis <sebastian.weis@gmx.at>
2022-12-03 21:09:00 +0100

    Initial project setup

---
+++ Solutions/Solution.cs
@@ -0,0 +1,15 @@
+namespace AdventOfCode2021.Solutions {
+    public abstract class Solution {
+        public abstract List<Action> Stages { get; }
+
+        protected List<string> ReadInputFile(string inputFileName) {
+            string path = @$".\Inputs\{inputFileName}";
+            if (!File.Exists(path)) {
+                throw new InvalidOperationException($"The input file '{path}' does not exist!");
+            }
+
+            return File.ReadAllLines(path).ToList();
+        }
+
+    }
+}
